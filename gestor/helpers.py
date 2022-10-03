a4a4a9d390565a93971deae18c4e642187d2c05f
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-03 10:18:07 +0200

    helpers

--- gestor/helpers.py
+++ gestor/helpers.py
@@ -3,3 +3,11 @@ import platform #detecta el sistema operativo que queremos y lo adapta para pode
 
 def limpiar_pantalla():
     os.system('cls') if platform.system() == "Windows" else os.system('clear')
+
+#Es para poder leer un texto cómodamente
+def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
+    print(mensaje) if mensaje else None
+    while True:
+        texto = input("> ")
+        if len(texto) >= longitud_min and len(texto) <= longitud_max:
+            return texto
